2c2420b25cab34222881c1454d0c5f7fcd270dd6
Ivan Lim <aimprove@gmail.com>
2018-01-09 13:18:04 -0500

    Modify footer credit link to https

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer navbar justify-content-end fixed-bottom text-muted bg-light">
-    Proudly brought to you by&nbsp;<a href='http://aiwebstudio.com' target='_blank'>Active Improvement Web Studio</a>.
+    Proudly brought to you by&nbsp;<a href='https://aiwebstudio.com' target='_blank'>Active Improvement Web Studio</a>.
 </footer>
