0257cae1de59974ca542e4715d88671cdd14152e
Ivan Lim <aimprove@gmail.com>
2018-01-09 13:15:58 -0500

    Remove footer styles

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,3 @@
-<footer
-    class="footer navbar justify-content-end fixed-bottom text-muted bg-light"
-    style="height: 2rem; line-height: 2rem;"
->
+<footer class="footer navbar justify-content-end fixed-bottom text-muted bg-light">
     Proudly brought to you by&nbsp;<a href='http://aiwebstudio.com' target='_blank'>Active Improvement Web Studio</a>.
 </footer>
